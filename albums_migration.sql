3b6141ec9df86d209efd6b5e4097409ce0c8f2cb
Jeremiah Matthews <jeremiah.matthews105@gmail.com>
2023-10-23 12:11:15 -0500

    sql tables exer.

---
+++ albums_migration.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  artist_name VARCHAR(50),
+  record_name  VARCHAR(100) NOT NULL,
+  release_date INT(4),
+  sales DECIMAL(3,1),
+  genre CHAR(50),
+  PRIMARY KEY (id)
+);
